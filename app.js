798786b93ee7070e205a660728e92aa08a5f2396
unknown <ishitamodi03.com>
2024-03-18 22:47:52 +0530

    ALL JSON TO API

--- app.js
+++ app.js
@@ -18,50 +18,111 @@ app.post("/",(req,res)=>{
   var Department=req.body.department;
   var PurposeOfVisit=req.body.Purpose_of_Visit;
 
-  // Travelling
-
-  var t=0;
-  var travelId=t++;
-  var travelDate = req.body.Date;
-  var travelFrom = req.body.Place;
-  var travelTo = req.body.Name;
-  var travelMode = req.body.Mode;
-  var travelClass = req.body.Class;
-  var travelFare = req.body.Fare;
-  var travelConveyance = req.body.Convenience;
-  var travelFoodAndLodging = req.body.Food_Loddging;
-  var travelIncidental = req.body.Incidential;
-  var travelTotal = req.body.Total;
 
   // Conveyance
-  var u=0;
-  var conveyanceId = u++;
-  var conveyanceDate = req.body.Convenience_Date;
-  var conveyanceFrom = req.body.Convenience_From;
-  var conveyanceTo = req.body.Convenience_To;
-  var conveyanceMode = req.body.Convenience_Mode;
-  var conveyancePurpose = req.body.Convenience_Purpose;
-  var conveyanceAmount = req.body.Convenience_Amount;
-  var conveyanceBill = req.body.Convenience_Bill;
+  var Convenience = [];
+  var totalRowsC = parseInt(req.body.totalRowsConvenience);
+
+  for (var i = 1; i <= totalRowsC ; i++) {
+    var conveyanceDate = req.body['Convenience_Date' + i];
+    var conveyanceFrom = req.body['Convenience_From' + i];
+    var conveyanceTo = req.body['Convenience_To' + i];
+    var conveyanceMode = req.body['Convenience_Mode' + i];
+    var conveyancePurpose = req.body['Convenience_Purpose' + i];
+    var conveyanceAmount = req.body['Convenience_Amount' + i];
+    var conveyanceBill = req.body['Convenience_Bill' + i];
+
+    Convenience.push({
+      conveyance_Id: i,
+      conveyance_Date: conveyanceDate,
+      conveyanc_From: conveyanceFrom,
+      conveyance_To: conveyanceTo,
+      conveyance_Mode: conveyanceMode,
+      conveyance_Purpose: conveyancePurpose,
+      conveyance_Amount: conveyanceAmount,
+      conveyance_Bill: conveyanceBill
+    });
+  }
 
   // foodAndLodging
-  var v=0;
-  var foodLodgingId = v++;
-  var foodLodgingDate = req.body.foodLodging_Date;
-  var foodLodgingBillNo = req.body.foodLodging_BillNo;
-  var foodLodgingHotel = req.body.foodLodging_Hotel;
-  var foodLodgingOccupancy = req.body.foodLodging_Occupancy;
-  var foodLodgingAmount = req.body.foodLodging_Amount;
-  var foodLodgingBill = req.body.foodLodging_Bill;
+  var foodLodgings = [];
+  var totalRowsF = parseInt(req.body.totalRowsFoodAndLodging);
+
+  for (var i = 1; i <= totalRowsF ; i++) {
+    var foodLodgingDate = req.body['foodLodging_Date' + i];
+    var foodLodgingBillNo = req.body['foodLodging_BillNo' + i];
+    var foodLodgingHotel = req.body['foodLodging_Hotel' + i];
+    var foodLodgingOccupancy = req.body['foodLodging_Occupancy' + i];
+    var foodLodgingAmount = req.body['foodLodging_Amount' + i];
+    var foodLodgingBill = req.body['foodLodging_Bill' + i];
+
+    foodLodgings.push({
+      foodLodging_Id: i,
+      foodLodging_Date: foodLodgingDate,
+      foodLodging_BillNo: foodLodgingBillNo,
+      foodLodging_Hotel: foodLodgingHotel,
+      foodLodging_Occupancy: foodLodgingOccupancy,
+      foodLodging_Amount: foodLodgingAmount,
+      foodLodging_Bill: foodLodgingBill
+    });
+  }
 
   // Incidental
-  var w=0; 
-  var incidentalId = w++;
-  var incidentalDate = req.body.incidentals_Date;
-  var incidentalExpense = req.body.incidentals_Expense;
-  var incidentalRemarks = req.body.incidentals_Remarks;
-  var incidentalAmount = req.body.incidentals_Amount;
-  var incidentalBill = req.body.incidentals_Bill;
+  var incidentals = [];
+  var totalRowsI = parseInt(req.body.totalRowsIncidental);
+
+  for (var i = 1; i <= totalRowsI ; i++) {
+    var incidentalDate = req.body['incidentals_Date' + i];
+    var incidentalExpense = req.body['incidentals_Expense' + i];
+    var incidentalRemarks = req.body['incidentals_Remarks' + i];
+    var incidentalAmount = req.body['incidentals_Amount' + i];
+    var incidentalBill = req.body['incidentals_Bill' + i];
+
+    incidentals.push({
+      incidental_Id: i ,
+      incidental_Date: incidentalDate,
+      incidental_Expense: incidentalExpense,
+      incidental_Remarks: incidentalRemarks,
+      incidental_Amount: incidentalAmount,
+      incidental_Bill: incidentalBill
+    });
+  }
+  
+  // Travelling
+  var travels = [];
+  var totalRowsT = parseInt(req.body.totalRowsTravelling);
+
+  for (var i = 1; i <= totalRowsT ; i++) {
+    var travelDate = req.body['Date' + i];
+    var travelFrom = req.body['From' + i];
+    var travelTo = req.body['To' + i];
+    var travelMode = req.body['Mode' + i];
+    var travelClass = req.body['Class' + i];
+    var travelFare = req.body['Fare' + i];
+    var travelConveyance = req.body['Convenience' + i];
+    var travelFoodAndLodging = req.body['Food_Lodging' + i];
+    var travelIncidental = req.body['Incidental' + i];
+    var travelTotal = req.body['Total' + i];
+
+    travels.push({
+      travel_Id: i,
+      travel_Date: travelDate,
+      travel_From: travelFrom,
+      travel_To: travelTo,
+      travel_Mode: travelMode,
+      travel_Class: travelClass,
+      travel_Fare: travelFare,
+      travel_Conveyance: travelConveyance,
+      travel_Food_Lodging: travelFoodAndLodging,
+      travel_Incidemtal: travelIncidental,
+      travel_Total: travelTotal,
+      travelDetails: {
+        conveyances: Convenience,
+        foodLodgings: foodLodgings,
+        incidentals: incidentals
+    }
+    });
+  }
 
   var data = {
     
@@ -71,57 +132,7 @@ app.post("/",(req,res)=>{
     designation: Designation,
     department: Department,
     purposeOfVisit: PurposeOfVisit,
-    travels: [
-      {
-        travel_Id:travelId,
-        travel_Date: travelDate,
-        travel_From: travelFrom,
-        travel_To: travelTo,
-        travel_Mode: travelMode,
-        travel_Class: travelClass,
-        travel_Fare: travelFare,
-        travel_Conveyance: travelConveyance,
-        travel_Food_Lodging: travelFoodAndLodging,
-        travel_Incidemtal: travelIncidental,
-        travel_Total: travelTotal,
-        travelDetails: {
-          conveyances: [
-              {
-                  conveyance_Id: conveyanceId,
-                  conveyance_Date: conveyanceDate,
-                  conveyanc_From: conveyanceFrom,
-                  conveyance_To: conveyanceTo,
-                  conveyance_Mode: conveyanceMode,
-                  conveyance_Purpose: conveyancePurpose,
-                  conveyance_Amount: conveyanceAmount,
-                  conveyance_Bill: conveyanceBill
-              }
-          ],
-          foodLodgings: [
-              {
-                  foodLodging_Id: foodLodgingId,
-                  foodLodging_Date: foodLodgingDate,
-                  foodLodging_BillNo: foodLodgingBillNo,
-                  foodLodging_Hotel: foodLodgingHotel,
-                  foodLodging_Occupancy: foodLodgingOccupancy,
-                  foodLodging_Amount: foodLodgingAmount,
-                  foodLodging_Bill: foodLodgingBill
-              }
-          ],
-          incidentals: [
-              {
-                  incidental_Id: incidentalId ,
-                  incidental_Date: incidentalDate,
-                  incidental_Expense: incidentalExpense,
-                  incidental_Remarks: incidentalRemarks,
-                  incidental_Amount: incidentalAmount,
-                  incidental_Bill: incidentalBill
-              }
-          ]
-      }
-        
-      },
-    ],
+    travels: travels
   };
 
   var jsonData=JSON.stringify(data);
