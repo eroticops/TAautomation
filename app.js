8a974f4334b956362e91a65563b6eed9f7f97a98
unknown <ishitamodi03.com>
2024-04-05 14:06:25 +0530

    5/4/24

--- app.js
+++ app.js
@@ -7,164 +7,164 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Functions
-function randomAlpha(length = 10) {
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
+// function randomAlpha(length = 10) {
+//   const characters =
+//     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//   let result = "";
+//   for (let i = 0; i < length; i++) {
+//     result += characters.charAt(Math.floor(Math.random() * characters.length));
+//   }
+//   return result;
+// }
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", (req, res) => {
-  var Name = req.body.Name;
-  var EmpId = req.body.empID;
-  var Location = req.body.location;
-  var Designation = req.body.designation;
-  var Department = req.body.department;
-  var PurposeOfVisit = req.body.Purpose_of_Visit;
+//   var Name = req.body.Name;
+//   var EmpId = req.body.empID;
+//   var Location = req.body.location;
+//   var Designation = req.body.designation;
+//   var Department = req.body.department;
+//   var PurposeOfVisit = req.body.Purpose_of_Visit;
 
-  // Conveyance
-  var Convenience = [];
-  var totalRowsC = parseInt(req.body.totalRowsConvenience);
+//   // Conveyance
+//   var Convenience = [];
+//   var totalRowsC = parseInt(req.body.totalRowsConvenience);
 
-  for (var i = 1; i <= totalRowsC ; i++) {
-    var conveyance_Date = req.body['Convenience_Date' + i];
-    var conveyance_From = req.body['Convenience_From' + i];
-    var conveyance_To = req.body['Convenience_To' + i];
-    var conveyance_Mode = req.body['Convenience_Mode' + i];
-    var conveyance_Purpose = req.body['Convenience_Purpose' + i];
-    var conveyance_Amount = req.body['Convenience_Amount' + i];
-    var conveyance_Bill = req.body['Convenience_BillId' + i];
+//   for (var i = 1; i <= totalRowsC ; i++) {
+//     var conveyance_Date = req.body['Convenience_Date' + i];
+//     var conveyance_From = req.body['Convenience_From' + i];
+//     var conveyance_To = req.body['Convenience_To' + i];
+//     var conveyance_Mode = req.body['Convenience_Mode' + i];
+//     var conveyance_Purpose = req.body['Convenience_Purpose' + i];
+//     var conveyance_Amount = req.body['Convenience_Amount' + i];
+//     var conveyance_Bill = req.body['Convenience_BillId' + i];
 
-    console.log(conveyance_Bill);
+//     console.log(conveyance_Bill);
 
-    Convenience.push({
-      conveyanceId: randomAlpha(),
-      conveyanceDate: conveyance_Date,
-      conveyanceFrom: conveyance_From,
-      conveyanceTo: conveyance_To,
-      conveyanceMode: conveyance_Mode,
-      conveyancePurpose: conveyance_Purpose,
-      conveyanceAmount: conveyance_Amount,
-      conveyanceBill: conveyance_Bill,
-    });
-  }
-  // foodAndLodging
-  var foodLodgings = [];
-  var totalRowsF = parseInt(req.body.totalRowsFoodAndLodging);
+//     Convenience.push({
+//       conveyanceId: randomAlpha(),
+//       conveyanceDate: conveyance_Date,
+//       conveyanceFrom: conveyance_From,
+//       conveyanceTo: conveyance_To,
+//       conveyanceMode: conveyance_Mode,
+//       conveyancePurpose: conveyance_Purpose,
+//       conveyanceAmount: conveyance_Amount,
+//       conveyanceBill: conveyance_Bill,
+//     });
+//   }
+//   // foodAndLodging
+//   var foodLodgings = [];
+//   var totalRowsF = parseInt(req.body.totalRowsFoodAndLodging);
 
-  for (var i = 1; i <= totalRowsF; i++) {
-    var foodLodging_Date = req.body["foodLodging_Date" + i];
-    var foodLodging_BillNo = req.body["foodLodging_BillNo" + i];
-    var foodLodging_Hotel = req.body["foodLodging_Hotel" + i];
-    var foodLodging_Occupancy = req.body["foodLodging_Occupancy" + i];
-    var foodLodging_Amount = req.body["foodLodging_Amount" + i];
-    var foodLodging_Bill = req.body["foodLodging_BillId" + i];
+//   for (var i = 1; i <= totalRowsF; i++) {
+//     var foodLodging_Date = req.body["foodLodging_Date" + i];
+//     var foodLodging_BillNo = req.body["foodLodging_BillNo" + i];
+//     var foodLodging_Hotel = req.body["foodLodging_Hotel" + i];
+//     var foodLodging_Occupancy = req.body["foodLodging_Occupancy" + i];
+//     var foodLodging_Amount = req.body["foodLodging_Amount" + i];
+//     var foodLodging_Bill = req.body["foodLodging_BillId" + i];
 
-    foodLodgings.push({
-      foodLodgingId: randomAlpha(),
-      foodLodgingDate: foodLodging_Date,
-      foodLodgingBillNo: foodLodging_BillNo,
-      foodLodgingHotel: foodLodging_Hotel,
-      foodLodgingOccupancy: foodLodging_Occupancy,
-      foodLodgingAmount: foodLodging_Amount,
-      foodLodgingBill: foodLodging_Bill,
-    });
-  }
-  // Incidental
-  var incidentals = [];
-  var totalRowsI = parseInt(req.body.totalRowsIncidental);
+//     foodLodgings.push({
+//       foodLodgingId: randomAlpha(),
+//       foodLodgingDate: foodLodging_Date,
+//       foodLodgingBillNo: foodLodging_BillNo,
+//       foodLodgingHotel: foodLodging_Hotel,
+//       foodLodgingOccupancy: foodLodging_Occupancy,
+//       foodLodgingAmount: foodLodging_Amount,
+//       foodLodgingBill: foodLodging_Bill,
+//     });
+//   }
+//   // Incidental
+//   var incidentals = [];
+//   var totalRowsI = parseInt(req.body.totalRowsIncidental);
 
-  for (var i = 1; i <= totalRowsI; i++) {
-    var incidental_Date = req.body["incidentals_Date" + i];
-    var incidental_Expense = req.body["incidentals_Expense" + i];
-    var incidental_Remarks = req.body["incidentals_Remarks" + i];
-    var incidental_Amount = req.body["incidentals_Amount" + i];
-    var incidental_Bill = req.body["incidentals_BillId" + i];
+//   for (var i = 1; i <= totalRowsI; i++) {
+//     var incidental_Date = req.body["incidentals_Date" + i];
+//     var incidental_Expense = req.body["incidentals_Expense" + i];
+//     var incidental_Remarks = req.body["incidentals_Remarks" + i];
+//     var incidental_Amount = req.body["incidentals_Amount" + i];
+//     var incidental_Bill = req.body["incidentals_BillId" + i];
 
-    incidentals.push({
-      incidentalId: randomAlpha(),
-      incidentalDate: incidental_Date,
-      incidentalExpense: incidental_Expense,
-      incidentalRemarks: incidental_Remarks,
-      incidentalAmount: incidental_Amount,
-      incidentalBill: incidental_Bill,
-    });
-  }
-  // Travelling
-  var travels = [];
-  var totalRowsT = parseInt(req.body.totalRowsTravelling);
+//     incidentals.push({
+//       incidentalId: randomAlpha(),
+//       incidentalDate: incidental_Date,
+//       incidentalExpense: incidental_Expense,
+//       incidentalRemarks: incidental_Remarks,
+//       incidentalAmount: incidental_Amount,
+//       incidentalBill: incidental_Bill,
+//     });
+//   }
+//   // Travelling
+//   var travels = [];
+//   var totalRowsT = parseInt(req.body.totalRowsTravelling);
 
-  for (var i = 1; i <= totalRowsT; i++) {
-    var travel_Date = req.body["Date" + i];
-    var travel_From = req.body["From" + i];
-    var travel_To = req.body["To" + i];
-    var travel_Mode = req.body["Mode" + i];
-    var travel_Class = req.body["Class" + i];
-    var travel_Fare = req.body["Fare" + i];
-    var travel_Conveyance = req.body["Convenience" + i];
-    var travel_FoodAndLodging = req.body["Food_Lodging" + i];
-    var travel_Incidental = req.body["Incidental" + i];
-    var travel_Total = req.body["Total" + i];
+//   for (var i = 1; i <= totalRowsT; i++) {
+//     var travel_Date = req.body["Date" + i];
+//     var travel_From = req.body["From" + i];
+//     var travel_To = req.body["To" + i];
+//     var travel_Mode = req.body["Mode" + i];
+//     var travel_Class = req.body["Class" + i];
+//     var travel_Fare = req.body["Fare" + i];
+//     var travel_Conveyance = req.body["Convenience" + i];
+//     var travel_FoodAndLodging = req.body["Food_Lodging" + i];
+//     var travel_Incidental = req.body["Incidental" + i];
+//     var travel_Total = req.body["Total" + i];
 
-    travels.push({
-      travelId: randomAlpha(),
-      travelDate: travel_Date,
-      travelFrom: travel_From,
-      travelTo: travel_To,
-      travelMode: travel_Mode,
-      travelClass: travel_Class,
-      travelFare: travel_Fare,
-      travelConveyance: travel_Conveyance,
-      travelFoodLodging: travel_FoodAndLodging,
-      travelIncidental: travel_Incidental,
-      travelTotal: travel_Total,
-      travelDetails: {
-        conveyances: Convenience,
-        foodLodgings: foodLodgings,
-        incidentals: incidentals,
-      },
-    });
-  }
+//     travels.push({
+//       travelId: randomAlpha(),
+//       travelDate: travel_Date,
+//       travelFrom: travel_From,
+//       travelTo: travel_To,
+//       travelMode: travel_Mode,
+//       travelClass: travel_Class,
+//       travelFare: travel_Fare,
+//       travelConveyance: travel_Conveyance,
+//       travelFoodLodging: travel_FoodAndLodging,
+//       travelIncidental: travel_Incidental,
+//       travelTotal: travel_Total,
+//       travelDetails: {
+//         conveyances: Convenience,
+//         foodLodgings: foodLodgings,
+//         incidentals: incidentals,
+//       },
+//     });
+//   }
 
-  var data = {
-    name: Name,
-    empId: EmpId,
-    location: Location,
-    designation: Designation,
-    department: Department,
-    purposeOfVisit: PurposeOfVisit,
-    travels: travels,
-  };
+//   var data = {
+//     name: Name,
+//     empId: EmpId,
+//     location: Location,
+//     designation: Designation,
+//     department: Department,
+//     purposeOfVisit: PurposeOfVisit,
+//     travels: travels,
+//   };
 
-  var jsonData = JSON.stringify(data);
+//   var jsonData = JSON.stringify(data);
 
-  var options = {
-    url: "https://user-be-6dvcrnuzba-uc.a.run.app/upload",
-    method: "POST",
-    body: jsonData,
-    headers: {
-      Authorization:
-        "Bearer 186ca51480a74b829cd593cb3e560d82410bdfc8cfcfd39fff7b0ad9f7015814",
-      "Content-Type": "application/json",
-    },
-  };
-  console.log(jsonData);
-  request(options, (error, response, body) => {
-    if (error) {
-      console.log(response.statusCode);
-      // console.log(response.body);
-    } else {
-      console.log(response.statusCode);
-      console.log(response.body);
-    }
-  });
+//   var options = {
+//     url: "https://user-be-6dvcrnuzba-uc.a.run.app/upload",
+//     method: "POST",
+//     body: jsonData,
+//     headers: {
+//       Authorization:
+//         "Bearer 186ca51480a74b829cd593cb3e560d82410bdfc8cfcfd39fff7b0ad9f7015814",
+//       "Content-Type": "application/json",
+//     },
+//   };
+//   console.log(jsonData);
+//   request(options, (error, response, body) => {
+//     if (error) {
+//       console.log(response.statusCode);
+//       // console.log(response.body);
+//     } else {
+//       console.log(response.statusCode);
+//       console.log(response.body);
+//     }
+//   });
   res.sendFile(__dirname + "/index.html");
 });
 
--- public/js/frontend_script.js
+++ public/js/frontend_script.js
@@ -215,3 +215,122 @@ function myCreateFunctionTravelling() {
           totalRowsInput.value = totalRows - 1;
       }
   }
+  
+function randomAlpha(length = 10) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+document.getElementById("yourFormId").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var formData = new FormData(this);
+
+  var data = {
+    name: formData.get("Name"),
+    empId: formData.get("empID"),
+    location: formData.get("location"),
+    designation: formData.get("designation"),
+    department: formData.get("department"),
+    purposeOfVisit: formData.get("Purpose_of_Visit"),
+    travels: []
+  };
+
+  var totalRowsT = parseInt(formData.get("totalRowsTravelling"));
+
+  for (var i = 1; i <= totalRowsT; i++) {
+    var travel = {
+      travelId: randomAlpha(),
+      travelDate: formData.get("Date" + i),
+      travelFrom: formData.get("From" + i),
+      travelTo: formData.get("To" + i),
+      travelMode: formData.get("Mode" + i),
+      travelClass: formData.get("Class" + i),
+      travelFare: formData.get("Fare" + i),
+      travelConveyance: formData.get("Convenience" + i),
+      travelFoodLodging: formData.get("Food_Lodging" + i),
+      travelIncidental: formData.get("Incidental" + i),
+      travelTotal: formData.get("Total" + i),
+      travelDetails: {
+        conveyances: [],
+        foodLodgings: [],
+        incidentals: []
+      }
+    };
+
+    var totalRowsC = parseInt(formData.get("totalRowsConvenience"));
+    for (var j = 1; j <= totalRowsC; j++) {
+      if (formData.has("Convenience_Date" + j)) {
+        travel.travelDetails.conveyances.push({
+          conveyanceId: randomAlpha(),
+          conveyanceDate: formData.get("Convenience_Date" + j),
+          conveyanceFrom: formData.get("Convenience_From" + j),
+          conveyanceTo: formData.get("Convenience_To" + j),
+          conveyanceMode: formData.get("Convenience_Mode" + j),
+          conveyancePurpose: formData.get("Convenience_Purpose" + j),
+          conveyanceAmount: formData.get("Convenience_Amount" + j),
+          conveyanceBill: formData.get("Convenience_BillId" + j)
+        });
+      }
+    }
+
+    var totalRowsF = parseInt(formData.get("totalRowsFoodAndLodging"));
+    for (var k = 1; k <= totalRowsF; k++) {
+      if (formData.has("foodLodging_Date" + k)) {
+        travel.travelDetails.foodLodgings.push({
+          foodLodgingId: randomAlpha(),
+          foodLodgingDate: formData.get("foodLodging_Date" + k),
+          foodLodgingBillNo: formData.get("foodLodging_BillNo" + k),
+          foodLodgingHotel: formData.get("foodLodging_Hotel" + k),
+          foodLodgingOccupancy: formData.get("foodLodging_Occupancy" + k),
+          foodLodgingAmount: formData.get("foodLodging_Amount" + k),
+          foodLodgingBill: formData.get("foodLodging_BillId" + k)
+        });
+      }
+    }
+
+    var totalRowsI = parseInt(formData.get("totalRowsIncidental"));
+    for (var l = 1; l <= totalRowsI; l++) {
+      if (formData.has("incidentals_Date" + l)) {
+        travel.travelDetails.incidentals.push({
+          incidentalId: randomAlpha(),
+          incidentalDate: formData.get("incidentals_Date" + l),
+          incidentalExpense: formData.get("incidentals_Expense" + l),
+          incidentalRemarks: formData.get("incidentals_Remarks" + l),
+          incidentalAmount: formData.get("incidentals_Amount" + l),
+          incidentalBill: formData.get("incidentals_BillId" + l)
+        });
+      }
+    }
+
+    data.travels.push(travel);
+  }
+
+  var jsonData = JSON.stringify(data);
+
+  fetch("https://user-be-6dvcrnuzba-uc.a.run.app/upload", {
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer 186ca51480a74b829cd593cb3e560d82410bdfc8cfcfd39fff7b0ad9f7015814",
+      "Content-Type": "application/json"
+    },
+    body: jsonData
+  })
+  .then(response => {
+    console.log("Response status:", response.status);
+    return response.json();
+  })
+  .then(data => {
+    console.log("Response data:", data);
+    // Handle response data as needed
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+});
+  
